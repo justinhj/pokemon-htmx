3e382ef9e80fb34163a43ade08bd2eb9fe3bf082
Justin Heyes-Jones <justinhj@gmail.com>
2023-09-24 13:04:28 -0700

    Add pug templating library to produce pokemon list

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import express, { Request, Response } from "express";
-import axios from "axios";
+import express, { Request, Response } from 'express';
+import axios from 'axios';
+import pug from 'pug';
 
 const app: express.Application = express();
 const port: number = 8080;
@@ -8,6 +9,14 @@ const POKE_BASE_URL: string = "https://pokeapi.co/api/v2/";
 const POKE_POKEMON_URL: string = `${POKE_BASE_URL}pokemon`;
 const POKEMON_PAGE_SIZE: number = 15;
 
+const pokemonListPugFn = pug.compile(`
+  each result in results
+    tr
+      td= result.name
+      td
+        a(href=result.url)= result.url
+  `);
+
 // add a static route to serve static files from a folder named "public"
 app.use(express.static("public"));
 
@@ -15,12 +24,8 @@ app.get("/pokemon", async (req: Request, res: Response) => {
   try {
     const pokemonPage: number = parseInt(req.query["pokemon-page"] as string) || 1;
     const url = `${POKE_POKEMON_URL}?offset=${(pokemonPage - 1) * POKEMON_PAGE_SIZE}&limit=${POKEMON_PAGE_SIZE}`;
-    // Use axios to call the endpoint above.
     let r = await axios.get(url);
-    let html = "";
-    r.data.results.forEach((result: any) => {
-      html += `<tr><td>${result.name}</td><td><a href="${result.url}">${result.url}</a></td></tr>`;
-    });
+    let html = pokemonListPugFn(r.data.results);
     res.send(html);
   } catch (error) {
     console.error(error);
