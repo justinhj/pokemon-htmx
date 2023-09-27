925d9c139cbf8d5f97aaf9fa45f74507fa895dcd
Justin Heyes-Jones <justinhj@gmail.com>
2023-09-27 10:23:28 -0700

    Content now manages its own buttons properly

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,21 @@ const port: number = 8080;
 
 const POKE_BASE_URL: string = "https://pokeapi.co/api/v2/";
 const POKE_POKEMON_URL: string = `${POKE_BASE_URL}pokemon`;
-const POKEMON_PAGE_SIZE: number = 15;
+const POKEMON_PAGE_SIZE: number = 10;
 
 // pug config (templating)
 app.set("view engine", "pug");
 app.set("views", "./pugs");
 
 // helper functions for the templates
-function doubleCount(count: number): number {
-  return count * 2;
+// https://pokeapi.co/api/v2/pokemon?offset=10&limit=10
+// http://localhost:8080/pokemon?offset=10&limit=10
+function convertUrl(urlString: string): string {
+  const url = new URL(urlString);
+  url.host = 'localhost:8080'; // TODO this should be the actual hostname
+  url.pathname = '/pokemon';
+  url.protocol = 'http';
+  return url.toString();
 }
 
 // add a static route to serve static files from a folder named "public"
@@ -23,10 +29,9 @@ app.use(express.static("public"));
 
 app.get("/pokemon", async (req: Request, res: Response) => {
   try {
-    const pokemonPage: number = parseInt(req.query["pokemon-page"] as string) || 1;
-    const url = `${POKE_POKEMON_URL}?offset=${(pokemonPage - 1) * POKEMON_PAGE_SIZE}&limit=${POKEMON_PAGE_SIZE}`;
+    const url = `${POKE_POKEMON_URL}?offset=${req.query['offset']}&limit=${req.query['limit']}`;
     let r = await axios.get(url);
-    res.render("pokemonlist", {data: r.data, doubleCount: doubleCount});
+    res.render("pokemonlist", {data: r.data, convertUrl: convertUrl});
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal Server Error");
